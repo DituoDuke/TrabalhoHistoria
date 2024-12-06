0a352afb934959f71af27919f51e7b11245fe303
Glitch (revoltas) <none>
2024-12-06 23:01:01 +0000

    ⚗️🥍 Updated with Glitch

--- routes/index.js
+++ routes/index.js
@@ -2,24 +2,34 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/", (req, res, next) => {
   res.render("index", {});
 });
 
-router.get("/contexto", function (req, res, next) {
+router.get("/contexto", (req, res, next) => {
   res.render("contexto", {});
 });
 
-router.get("/diferenca", function (req, res, next) {
+router.get("/diferenca", (req, res, next) => {
   res.render("diferenca", {});
 });
 
-router.get("/religiao", function (req, res, next) {
+router.get("/religiao",  (req, res, next) => {
   res.render("religiao", {});
 });
 
-router.get("/resultado", function (req, res, next) {
+router.get("/resultado",  (req, res, next) => {
   res.render("resultado", {});
 });
+
+router.get("/hipoteses", (req, res, next) => {
+  res.render("utopia", {});
+});
+
+router.get("/ferrou",  (req, res, next) => {
+  res.render("fudeu", {});
+});
+
+
 
 module.exports = router;
